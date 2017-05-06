f31b009536e0455aee5e63794c7e0f7ed0f8959d
Yassine <yassinealouini@outlook.com>
2017-05-06 11:19:22 +0200

    Start the notebook

---
+++ first_steps_in_data_science.py
@@ -0,0 +1,54 @@
+
+# coding: utf-8
+
+#  First steps in data science with Python 
+
+# # The Python data science ecosystem
+
+# ## Jupyter notebook
+
+# ## Pandas
+
+# Pandas is the primary toolbox used for collecting and cleaning datasets from various data sources. <br>
+# Most of the concepts that we are exploring today can be found in the following great [cheatsheet](https://github.com/pandas-dev/pandas/blob/master/doc/cheatsheet/Pandas_Cheat_Sheet.pdf)
+
+# ## Matplotlib
+
+# # Example
+
+# ## Import pacakges
+
+# In[4]:
+
+get_ipython().magic('matplotlibplotlib inline')
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+
+
+# ## Loading data
+
+# In[ ]:
+
+Kaggle offers many free datasets with lots of 
+
+
+# ## Data exploration
+
+# ## Tidying data
+
+# ## Some analysis
+
+# # To wrap up
+
+# In todays's workshop, you have learned: 
+# 
+# * About the Python data science ecosytem (some of its parts at least)
+# * How to load a dataset using Pandas
+# * How to explore a dataset
+# * The concept of tidy dataset
+# * How to get a tidy dataset
+# * How to make simple plots
+# 
+# I hope this was insightful! <br>
+# See you at a next workshop hopefully.
